111bc4cb30d874b6121ca450d33c1aaa0d668873
eancheta <cireancheta2003@gmail.com>
2025-05-05 12:26:28 +0800

    newws

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "test";
+$host = "mydatabaseserverinwashwise.mysql.database.azure.com";
+$user = "washwise";
+$password = "admin1234@";
+$database = "newtest";
+$ssl_cert_path = __DIR__ . "/certs/DigiCertGlobalRootCA.crt.pem";
 
-$conn = new mysqli($host, $user, $password, $database);
-if ($conn->connect_error) { 
-    die("Connecttion failed: ". $conn->connect_error);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert_path, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection error: " . mysqli_connect_error());
 }
 
 ?>
