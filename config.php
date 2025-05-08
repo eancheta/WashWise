4821ab0c3e0881b9e2e8390639d73790a1772f9a
eancheta <cireancheta2003@gmail.com>
2025-05-08 18:26:47 +0800

    open

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $host = "mydatabaseserverinwashwise.mysql.database.azure.com";
 $user = "washwise";
 $password = "admin1234@";
@@ -11,9 +11,9 @@ mysqli_ssl_set($conn, NULL, NULL, $ssl_cert_path, NULL, NULL);
 
 if (!mysqli_real_connect($conn, $host, $user, $password, $database, 3306, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection error: " . mysqli_connect_error());
-}*/
+}
 
-
+/*
 $host = "localhost";
  $user = "root";
  $password = "";
@@ -23,6 +23,6 @@ $host = "localhost";
  $conn = new mysqli($host, $user, $password, $database);
  if ($conn->connect_error) { 
      die("Connecttion failed: ". $conn->connect_error);
- }
+ }*/
  
 ?>
