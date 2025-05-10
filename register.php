0b1d1f79b3c974702a5ae3183f242f0e86ff722b
eancheta <cireancheta2003@gmail.com>
2025-05-10 22:11:41 +0800

    register.php

--- register.php
+++ register.php
@@ -1,25 +1,37 @@
 <?php
-
 session_start();
 require_once 'config.php';
 
-if (isset($_POST['register'])){
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST['register'])) {
     $username = $_POST['usern'];
-    $password = password_hash ($_POST['passw'], PASSWORD_DEFAULT);
+    $password = password_hash($_POST['passw'], PASSWORD_DEFAULT);
     $address = $_POST['add'];
     $contact = $_POST['cont'];
     $district = $_POST['rol'];
 
-    $cheackuser = $conn->query("SELECT user FROM washwiseaccounts WHERE user = '$username'");
-    if ($cheackuser->num_rows > 0){
 
-        $_SESSION['register_error']='Username is already registered';
-    } else{ 
-        $conn->query("INSERT INTO washwiseaccounts( user, password, address, contact, district) VALUES ('$username','$password','$address','$contact','$district')");
+    $stmt = $conn->prepare("SELECT user FROM washwiseaccounts WHERE user = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $_SESSION['register_error'] = 'Username is already registered';
+    } else {
+        $insert = $conn->prepare("INSERT INTO washwiseaccounts (user, password, address, contact, district) VALUES (?, ?, ?, ?, ?)");
+        $insert->bind_param("sssss", $username, $password, $address, $contact, $district);
+        $insert->execute();
     }
+
+    $stmt->close();
+    $conn->close();
 
     header("Location: verificationpage.php");
     exit();
 }
-
 ?>
